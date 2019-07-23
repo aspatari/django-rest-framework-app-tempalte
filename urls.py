8fc85344cf7b7887633fa2cfc7972cbcc5a376ba
Artur Spatari <artur.spatari@gmail.com>
2019-07-23 10:42:27 +0300

    reformat file

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 from rest_framework import routers
-router = routers.DefaultRouter(trailing_slash=False)
-urlpatterns = [
 
-] + router.urls
+router = routers.DefaultRouter(trailing_slash=False)
+urlpatterns = [] + router.urls
--- views.py
+++ views.py
@@ -3,4 +3,3 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import generics, viewsets, permissions
 from rest_framework.decorators import action
-
